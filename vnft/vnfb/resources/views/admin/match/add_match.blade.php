b169e6ce33ce13e619a4421fc0ba424c47e608ed
Ho Thang Trinh <hothangtrinh200902@gmail.com>
2022-09-08 17:16:54 +0700

    add default slide button to add match page/ admin

--- vnft/vnfb/resources/views/admin/match/add_match.blade.php
+++ vnft/vnfb/resources/views/admin/match/add_match.blade.php
@@ -3,7 +3,70 @@
 @section('head_icon', 'newspaper')
 @section('heading', 'Add Match')
 @section('admin_content')
+<style>
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 17px;
+}
 
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 13px;
+  width: 13px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #24caa1;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #24caa1;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(13px);
+  -ms-transform: translateX(13px);
+  transform: translateX(13px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 17px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
 <!-- Single pro tab start-->
 <div class="single-product-tab-area mg-b-30">
     <!-- Single pro tab review Start-->
@@ -67,16 +130,71 @@
                                                     <input name="stadium_background" type="file" class="form-control" placeholder="Stadium Background" accept="image/*" onchange="loadDemoImgFile(event,1)">
                                                     <img id="output1" style="max-width:50px">
                                                 </div>
-                                                <select name="ticket" class="form-control pro-edt-select form-control-primary">
-                                                    <option value="0" selected>No ticket sales</option>
-                                                    <option value="1">Tickets sales</option>
-                                                </select>
                                                 <div class="input-group mg-b-pro-edt">
                                                     <span class="input-group-addon">
                                                         <!-- <i class="icon nalika-user" aria-hidden="true"></i> -->
                                                         <i class="material-icons-outlined" aria-hidden="true">merge_type</i>
                                                     </span>
                                                     <input name="type" type="text" class="form-control" placeholder="Type">
+                                                </div>
+                                                <select name="ticket" class="form-control pro-edt-select form-control-primary">
+                                                    <option value="0" selected>No ticket sales</option>
+                                                    <option value="1">Tickets sales</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="review-content-section">
+                                                <h4>Ticket Price</h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="review-content-section">
+                                                <label class="switch">
+                                                    <input type="checkbox">
+                                                    <span class="slider round"></span>
+                                                </label>
+                                                 Default Price
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="review-content-section">
+                                                <div class="input-group mg-b-pro-edt">
+                                                    <span class="input-group-addon">
+                                                        <!-- <i class="icon nalika-user" aria-hidden="true"></i> -->
+                                                        <i class="material-icons-outlined" aria-hidden="true">class</i>
+                                                    </span>
+                                                    <input name="class1" type="text" class="form-control" placeholder="Class 1st ticket price">
+                                                </div>
+                                                <div class="input-group mg-b-pro-edt">
+                                                    <span class="input-group-addon">
+                                                        <!-- <i class="icon nalika-user" aria-hidden="true"></i> -->
+                                                        <i class="material-icons-outlined" aria-hidden="true">class</i>
+                                                    </span>
+                                                    <input name="class1" type="text" class="form-control" placeholder="Class 2nd ticket price">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="review-content-section">
+                                                <div class="input-group mg-b-pro-edt">
+                                                    <span class="input-group-addon">
+                                                        <!-- <i class="icon nalika-user" aria-hidden="true"></i> -->
+                                                        <i class="material-icons-outlined" aria-hidden="true">class</i>
+                                                    </span>
+                                                    <input name="class1" type="text" class="form-control" placeholder="Class 3rd ticket price">
+                                                </div>
+                                                <div class="input-group mg-b-pro-edt">
+                                                    <span class="input-group-addon">
+                                                        <!-- <i class="icon nalika-user" aria-hidden="true"></i> -->
+                                                        <i class="material-icons-outlined" aria-hidden="true">class</i>
+                                                    </span>
+                                                    <input name="class1" type="text" class="form-control" placeholder="Class 4th ticket price">
                                                 </div>
                                             </div>
                                         </div>
